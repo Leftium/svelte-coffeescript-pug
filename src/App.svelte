1ed96190482745cdcb6dc2ad50f6b36e2c14da21
Leftium <john@leftium.com>
2021-01-25 16:34:08 +0900

    Add reactive button sample to App.svelte.

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,52 @@
 <script lang="coffeescript">
   export name = null
-  console.log 'Hello Svelte-CoffeeScript'
+
+  ## Counter sample based on https://svelte.dev/tutorial/reactive-statements
+  count = 0
+ 
+  ## Mark Svelte reactive CoffeeScript statements by escaping out to JavaScript.
+  ## (Normally CoffeeScript syntax is incompatible with Svelte Reactive statements.)
+
+  `$: {`  ## Start Svelte reactive block.
+  if count >= 10
+    alert 'count is dangerously high!'
+    count = 9
+  `}`     ## End reactive block.
+
+  handleClick = () ->
+    count += 1
 </script>
 
 <template lang="pug">
-main
+  main
     h1 Hello {name}!
     p.
-        Visit the #[a(href='https://svelte.dev/tutorial') Svelte tutorial]
-        to learn how to build Svelte apps.
+      Visit the #[a(href='https://svelte.dev/tutorial') Svelte tutorial]
+      to learn how to build Svelte apps.
+
+    // How reactive buttons are done in Svelte + Pug.
+    // Note CoffeeScript cannot be used here.
+    button(on:click='{handleClick}') Clicked {count} {count === 1 ? 'time' : 'times'}
 </template>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
